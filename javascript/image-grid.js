3be599d4c95e8e9eb72cb3d75b2f511aeac0f438
oriolbaron <oriolbaron@uoc.edu>
2023-04-29 02:41:39 +0200

    commit-031

---
+++ javascript/image-grid.js
@@ -0,0 +1,8 @@
+const images = [
+    "/img/eiffel.jpg",
+    "/img/louvre.jpg",
+    "/img/montmartre.jpg",
+    "/img/paris-bici.jpg",
+    "/img/paris-gratis.jpg",
+    "img/paris-lgtb.jpg",
+  ];
