faec24430f2a508d3279b075ff24f6fd0c970a79
oriolbaron <131920634+oriolbaron@users.noreply.github.com>
2023-05-29 13:20:55 +0200

    commit-130 delete js

--- javascript/responsive-nav.js
+++
@@ -1,8 +0,0 @@
-function responsiveNav() {
-    var x = document.getElementById("nav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
-  }
