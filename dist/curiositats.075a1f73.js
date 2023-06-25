3bb50b503e2b6b0ef34c7064aa18d74c8c754285
oriolbaron <131920634+oriolbaron@users.noreply.github.com>
2023-06-25 20:38:56 +0200

    commit-156 update dist

---
+++ dist/curiositats.075a1f73.js
@@ -0,0 +1,13 @@
+var slideIndex = 0;
+showSlides();
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("slideshow")[0].getElementsByTagName("img");
+    for(i = 0; i < slides.length; i++)slides[i].classList.remove("active");
+    slideIndex++;
+    if (slideIndex > slides.length) slideIndex = 1;
+    slides[slideIndex - 1].classList.add("active");
+    setTimeout(showSlides, 7000); // Change image every 7 seconds
+}
+
+//# sourceMappingURL=curiositats.075a1f73.js.map
